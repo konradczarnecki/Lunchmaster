b0cf4be1c7ff59619cb92bdba9886f46c2d28951
Mikolaj Slefarski <mikolaj.slefarski@gmail.com>
2017-10-18 13:14:57 +0200

    Removed one unused import from LunchServiceTest

--- src/test/java/com/lunchmaster/api/lunch/service/LunchServiceTest.java
+++ src/test/java/com/lunchmaster/api/lunch/service/LunchServiceTest.java
@@ -13,19 +13,19 @@ import com.lunchmaster.api.user.dto.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 public class LunchServiceTest {

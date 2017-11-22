928981255f09400239a4c9596ccab77237ab3b8a
Mikolaj Slefarski <mikolaj.slefarski@gmail.com>
2017-11-22 16:47:06 +0100

    Add updated DB Dump v_0_1_2_FullCRUD

---
+++ src/main/resources/database/v_0_1_2_FullCRUD/structure_and_data/lunchmaster_lunch.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: lunchmaster
+-- ------------------------------------------------------
+-- Server version	5.6.36-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `lunch`
+--
+
+DROP TABLE IF EXISTS `lunch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lunch` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lunchmaster_id` int(11) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `deadline` datetime NOT NULL,
+  `expected_delivery` int(11) NOT NULL,
+  `rst_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id_idx_2` (`lunchmaster_id`),
+  KEY `rst_id_idx` (`rst_id`),
+  CONSTRAINT `FKp2epskrqa5p32ka1hjlo35jr3` FOREIGN KEY (`lunchmaster_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKr5cbw9dy7nusm1x1kt4raj892` FOREIGN KEY (`rst_id`) REFERENCES `restaurant` (`id`),
+  CONSTRAINT `lunchmaster_id` FOREIGN KEY (`lunchmaster_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `rst_id` FOREIGN KEY (`rst_id`) REFERENCES `restaurant` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lunch`
+--
+
+LOCK TABLES `lunch` WRITE;
+/*!40000 ALTER TABLE `lunch` DISABLE KEYS */;
+INSERT INTO `lunch` VALUES (1,1,'OPEN','2017-10-13 12:00:00',30,1),(2,2,'OPEN','2017-10-13 12:30:00',15,2);
+/*!40000 ALTER TABLE `lunch` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-22 16:44:16

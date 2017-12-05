ecf8b391dfc845e0a4829b33acd92cce941f6d39
Aleksander Kołata <olek.tbg.konta@gmail.com>
2017-12-05 21:09:48 +0100

    migration scripts added

--- src/main/java/com/lunchmaster/LunchmasterApplication.java
+++ src/main/java/com/lunchmaster/LunchmasterApplication.java
@@ -1,6 +1,5 @@
 package com.lunchmaster;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

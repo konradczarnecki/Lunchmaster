77bc7605088114d0fbf80828c70c4f1c496c02e2
k.czarnecki <k.czarnecki@partner.samsung.com>
2017-12-28 11:06:01 +0100

    Add material to restaurant module

--- front/src/app/restaurant/restaurant.module.ts
+++ front/src/app/restaurant/restaurant.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {RestaurantsComponent} from "./restaurants.component";
-import {RestaurantService} from "./service/restaurant.service";
-import {RestaurantTileComponent} from "./retaurant-tile/restaurant-tile.component";
-import {EditRestaurantComponent} from "./edit-restaurant/edit-restaurant.component";
-import {RestaurantDetailsComponent} from "./restaurant-details/restaurant-details.component";
+import { RestaurantsComponent } from './restaurants.component';
+import { RestaurantService } from './service/restaurant.service';
+import { RestaurantTileComponent } from './retaurant-tile/restaurant-tile.component';
+import { EditRestaurantComponent } from './edit-restaurant/edit-restaurant.component';
+import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
+import { MatButtonModule } from '@angular/material';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule
   ],
   declarations: [ RestaurantsComponent, RestaurantTileComponent, EditRestaurantComponent, RestaurantDetailsComponent ],
   providers: [ RestaurantService ]
